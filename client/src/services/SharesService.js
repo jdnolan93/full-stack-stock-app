b3c7ea0cdaf7329dbdc20620f2d12d177b6a4a91
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-01-21 13:51:37 +0000

    added services functions

--- client/src/services/SharesService.js
+++ client/src/services/SharesService.js
@@ -1,7 +1,22 @@
-import React from 'react';
+const baseURL = 'http://localhost:5000/api/shares/'
 
-const SharesService = () => {
-  return <div></div>;
-};
+export const getShares = () => {
+    return fetch(baseURL)
+        .then(res => res.json())
+}
 
-export default SharesService;
+export const postShare = (payload) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+}
+
+export const deleteShare = (id) => {
+    return fetch(baseURL + id, {
+        method: 'DELETE'
+    })
+}
+
