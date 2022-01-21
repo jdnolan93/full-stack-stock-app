b8d14d7f7b77c510dbd8a9db1edff35a661ab70a
Campbell Forsyth <camsh69@gmail.com>
2022-01-21 12:04:07 +0000

    sever and DB set up

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-const x = "hi"
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const createRouter = require('./helpers/create_router.js')
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('sharesApp');
+    const sharesCollection = db.collection('shares');
+    const sharesRouter = createRouter(sharesCollection);
+    app.use('/api/shares', sharesRouter);
+  })
+  .catch(console.err);
+
+app.listen(5000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
