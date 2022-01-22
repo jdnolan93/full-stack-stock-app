6cc0f4ce839358e96785c44efb368060ffa3aadc
Jamie <jamienolan93@hotmail.co.uk>
2022-01-22 18:02:02 +0000

    basic setup

---
+++ client/src/components/ShareCard.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export const ShareCard = () => {
+  return <div> 
+      <h1> hiya </h1>
+  </div>;
+};
---
+++ client/src/components/ShareList.js
@@ -0,0 +1,9 @@
+import { ShareCard } from "./ShareCard";
+
+import React from 'react';
+
+export const ShareList = () => {
+  return <div>
+      hey
+  </div>;
+};
