f7e9f1062fea31f8c12706d10efb9d2594af35a2
Jamie <jamienolan93@hotmail.co.uk>
2022-01-25 08:34:08 +0000

    changed edit button

--- client/src/components/ShareCard.js
+++ client/src/components/ShareCard.js
@@ -1,22 +1,40 @@
 import SharesAppContainer from '../containers/SharesAppContainer';
 import { deleteShare } from '../SharesService';
 import ShareUpdateNumber from './ShareUpdateNumber';
+import { useState } from 'react';
 
 const ShareCard = ({share, removeShare}) => {
-
+  const [showForm, setShowForm] = useState(false)
   const handleDelete = () => {
       deleteShare(share._id).then(()=>{
           removeShare(share._id);
       })
   }
+  const handleEdit = () => {
+      setShowForm(true)
+  }
+  if(showForm){
+
   return <ul> 
       <h1> {share.name} </h1>
       <p> {share.noOfShares}</p>
       <p> {share.symbol} </p>
       <ShareUpdateNumber id = {share._id}/>
       <button onClick={handleDelete}><i className="fas fa-trash"></i></button>
-      <button onClick> <i className="fas fa-edit"></i> </button>
+      <button onClick={handleEdit}> <i className="fas fa-edit"></i> </button>
   </ul>;
-};
+}
 
+  else{
+    return (
+        <ul> 
+      <h1> {share.name} </h1>
+      <p> {share.noOfShares}</p>
+      <p> {share.symbol} </p>
+      <button onClick={handleDelete}><i className="fas fa-trash"></i></button>
+      <button onClick={handleEdit}> <i className="fas fa-edit"></i> </button>
+      </ul>
+    )
+}
+} 
 export default ShareCard;
